d194c9115bbb74a998a07143956ea857e46d6fd4
Juan Diego Mateus <jmateus842@gmail.com>
2025-06-18 21:17:48 -0500

    progreso parcial del proyecto

--- el-argumentista/src/app/pages/acerca-de/acerca-de.ts
+++ el-argumentista/src/app/pages/acerca-de/acerca-de.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-acerca-de',
-  imports: [],
+  imports: [CommonModule, RouterLink],
   templateUrl: './acerca-de.html',
   styleUrl: './acerca-de.scss'
 })
--- el-argumentista/src/app/pages/inicio/inicio.ts
+++ el-argumentista/src/app/pages/inicio/inicio.ts
@@ -1,19 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { map, Observable } from 'rxjs';
 import { ExternalApi } from '../../services/external-api';
 
 @Component({
   selector: 'app-inicio',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './inicio.html',
   styleUrl: './inicio.scss'
 })
 export class Inicio implements OnInit {
 
+  citas$!: Observable<any[]>;
+
   constructor(private api: ExternalApi) {}
 
   ngOnInit(): void {
-    this.api.obtenerCitasFilosoficas().subscribe(data => {
-      console.log('Citas filosoficas:', data);
-    });
+    // Obtenemos las citas y nos quedamos solo con el arreglo interno
+    this.citas$ = this.api.obtenerCitasFilosoficas().pipe(
+      map((resp) => resp.citas_filosoficas as any[])
+    );
   }
 }
